a4c994e2fa90329ffc977df34fbd5ea52e6330ef
Blake Conrad <bmconrad@iupui.edu>
2018-05-15 05:12:26 -0500

    Caret exploration

--- code/1_initial_findings__(20180512)/main_team_analysis.R
+++ code/1_initial_findings__(20180512)/main_team_analysis.R
@@ -35,64 +35,83 @@ df_test <- df_test %>% select(-Name)
 df_train_imputed <- df_train_imputed %>% select(-Name)
 df_test_imputed <- df_test_imputed %>% select(-Name)
 
+str(df_train)
 
-# partitian data
-inTrain <- createDataPartition(Sonar$Class,
-                               p=0.75,
-                               list=FALSE)
-training = Sonar[inTrain,]
-testing = Sonar[-inTrain,]
+df_train[,2:ncol(df_train)] <- df_train %>% select(-IC50) %>% 
+                              apply(2, function(x) as.numeric(x))
 
+df_test[,2:ncol(df_test)] <- df_test %>% select(-IC50) %>% 
+                              apply(2, function(x) as.numeric(x))
+
+df_train_imputed[,2:ncol(df_train_imputed)] <- df_train_imputed %>% select(-IC50) %>% 
+                                                apply(2, function(x) as.numeric(x))
+
+df_test_imputed[,2:ncol(df_test_imputed)] <- df_test_imputed %>% 
+                                             select(-IC50) %>% apply(2, function(x) as.numeric(x))
+
+
+  
 # control
 ctrl = trainControl(method="repeatedcv",
                     repeats=10,
-                    classProbs = TRUE,
-                    summaryFunction = twoClassSummary)
-# gridspace
-rdaGrid <- data.frame(expand.grid((0:4)/5, 0.5:2.5)) 
-names(rdaGrid) <- c("gamma", "lambda")
-rdaGrid
+                    number = 3,
+                    summaryFunction = defaultSummary)
 
-knnGrid <- data.frame(k=(1:25))
+# gridspace
+knnGrid <- data.frame(k = seq(3:25)) #knn
+leapSeqGrid <- data.frame(nvmax = seq(10:(ncol(df_train))))
+bagEarthGrid <- data.frame(expand.grid(nprune=c(1,10,100),degree= c(1:5)))
 
 # model 1 
 set.seed(123)
-plsFit <- train(Class ~.,
-                data = training,
-                method = c("pls"),
-                preProc = c("center", "scale"),
-                tuneLength = 15,
-                trControl = ctrl,
-                metric="ROC")
-ggplot(plsFit)
+
+knnFit <- train(IC50 ~.,
+                data = df_train,
+                method = c("knn"),
+                preProc = c("center", "scale","BoxCox" ,"nzv"),
+                tuneGrid = knnGrid,
+                trControl = ctrl)
+ggplot(knnFit)
+knnFit
 
 # model 2
 set.seed(123)
-rdaFit <- train(Class ~.,
-                data = training,
-                method = c("rda"),
-                preProc = c("center", "scale"),
-                tuneGrid=rdaGrid,
-                trControl = ctrl,
-                metric="ROC")
-ggplot(rdaFit)
+marsFit <- train(IC50 ~.,
+                 data = df_train,
+                 method = c("badEarth"),
+                 preProc = c("center", "scale","BoxCox" ,"nzv"),
+                 tuneGrid = bagEarthGrid,
+                 trControl = ctrl)
+ggplot(marsFit)
 
 # model 3
 set.seed(123)
-knnFit <- train(Class ~.,
-                data=training,
-                method=c("knn"),
-                preProc=c("center", "scale"),
-                tuneGrid=knnGrid,
-                trControl = ctrl,
-                metric="ROC")
-knnFit
-ggplot(knnFit)
+lmMixedFit <- train(IC50 ~.,
+                data = df_train,
+                method = c("leapSeq"),
+                preProc = c("center", "scale","BoxCox" ,"nzv"),
+                tuneGrid = leapSeqGrid,
+                trControl = ctrl)
+
+ggplot(lmMixedFit)
+
+# Model 4
+set.seed(123)
+svmRadialFit <- train(IC50 ~.,
+                    data = df_train,
+                    method = c("svmRadial"),
+                    preProc = c("center", "scale","BoxCox" ,"nzv"),
+                    tuneLength = 10,
+                    trControl = ctrl)
+
+ggplot(svmRadialFit)
+svmRadialFit
 
 # Resampling
 resamps <- resamples(list(knn=knnFit,
-                          pls=plsFit,
-                          rda=rdaFit))
+                          mars=marsFit,
+                          lmMixed=lmMixedFit,
+                          svmRadial=svmRadialFit))
 summary(resamps)
 
 # Are the models statistically different?
